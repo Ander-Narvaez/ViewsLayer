6457a6e68057ffb05ecb95111fccddddb5b442e2
Ander <57125158+Ander-Narvaez@users.noreply.github.com>
2021-04-01 20:29:15 -0600

    Agregar archivos de proyecto.

---
+++ WebFormEmpresa.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewsLayer
+{
+    public partial class WebFormEmpresa : System.Web.UI.Page
+    {
+        ServiceReference.WebServiceSoapClient Ws = new ServiceReference.WebServiceSoapClient();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String vResfrescarObjeto;
+            if (IsPostBack)
+            {
+                vResfrescarObjeto = "nueva";
+            }
+            else
+            {
+                cargarTabla();
+            }
+        }
+
+        public void cargarTabla()
+        {
+            string m = "";
+            try
+            {
+                DataSet Dts = new DataSet();
+                Dts = Ws.GetListEmpresa("", "", "", "", "", "S");
+                
+                tbl.DataSource = Dts;
+                tbl.DataBind();
+            }
+            catch (Exception es)
+            {
+                m = es.Message;
+            }
+        }
+
+        public void OnModal(int op)
+        {
+            if (op == 1)
+            {
+                string encenderModal = "$('#staticBackdrop').modal('show')";
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", encenderModal, true);
+            }
+            else if (op == 1)
+            {
+            }
+        }
+
+        protected void btnCrear_Click(object sender, EventArgs e)
+        {
+            TxtEMPRESA.Text = "";
+            TxtNOMBRE.Text = "";
+            TxtUBICACION.Text = "";
+            txtEMAIL.Text = "";
+            txtTELEFONO.Text = "";
+
+            alertModal.Visible = false;
+            btnEliminar.Visible = false;
+            btnSempleadoAgregar.Visible = false;
+
+            OnModal(1);
+        }
+
+        protected void tblEmpresas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow row = tbl.Rows[tbl.SelectedIndex];
+            try
+            {
+                TxtEMPRESA.Text = row.Cells[1].Text;
+                TxtNOMBRE.Text = row.Cells[2].Text;
+                TxtUBICACION.Text = row.Cells[3].Text;
+                txtEMAIL.Text = row.Cells[4].Text;
+                txtTELEFONO.Text = row.Cells[5].Text;
+               
+                btnEliminar.Visible = true;
+                tbl.SelectedIndex = -1;
+                alertModal.Visible = false;
+
+                OnModal(1);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+            }
+            catch (Exception)
+            {
+               
+            }
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        protected void btnSempleadoAgregar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebFormEmpresa.aspx.designer.cs
@@ -0,0 +1,224 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ViewsLayer
+{
+
+
+    public partial class WebFormEmpresa
+    {
+
+        /// <summary>
+        /// Control lblbebe.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblbebe;
+
+        /// <summary>
+        /// Control lblRelacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblRelacion;
+
+        /// <summary>
+        /// Control btnCrear.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCrear;
+
+        /// <summary>
+        /// Control tbl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView tbl;
+
+        /// <summary>
+        /// Control AlertFooter.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl AlertFooter;
+
+        /// <summary>
+        /// Control textFijo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label textFijo;
+
+        /// <summary>
+        /// Control informacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label informacion;
+
+        /// <summary>
+        /// Control lblTitleModal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTitleModal;
+
+        /// <summary>
+        /// Control lblempresa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblempresa;
+
+        /// <summary>
+        /// Control TxtEMPRESA.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtEMPRESA;
+
+        /// <summary>
+        /// Control lblnombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblnombre;
+
+        /// <summary>
+        /// Control TxtNOMBRE.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtNOMBRE;
+
+        /// <summary>
+        /// Control lblubicacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblubicacion;
+
+        /// <summary>
+        /// Control TxtUBICACION.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtUBICACION;
+
+        /// <summary>
+        /// Control lblemail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblemail;
+
+        /// <summary>
+        /// Control txtEMAIL.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEMAIL;
+
+        /// <summary>
+        /// Control lbltelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbltelefono;
+
+        /// <summary>
+        /// Control txtTELEFONO.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTELEFONO;
+
+        /// <summary>
+        /// Control alertModal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl alertModal;
+
+        /// <summary>
+        /// Control lbLAlertModal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbLAlertModal;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+
+        /// <summary>
+        /// Control btnEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminar;
+
+        /// <summary>
+        /// Control btnSempleadoAgregar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSempleadoAgregar;
+    }
+}
