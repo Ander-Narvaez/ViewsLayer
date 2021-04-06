00bae09359408895893dadbf532c930062036225
Elieth Rodriguez <eliethrodriguezmelendez@gmail.com>
2021-04-06 00:34:51 -0600

    commit WebFormBodega

--- WebFormBodega.aspx.designer.cs
+++ WebFormBodega.aspx.designer.cs
@@ -156,7 +156,7 @@ namespace ViewsLayer
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtInventario;
+        protected global::System.Web.UI.WebControls.DropDownList txtInventario;
 
         /// <summary>
         /// lblnombre control.
